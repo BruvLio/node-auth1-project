f275850a6fa6cbc716fd891a035e29707922a75b
BruvLio <lionel.mbayu@gmail.com>
2023-05-18 16:00:59 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,8 +6,12 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    next({ status: 401, message: "You shall not pass!" });
+  }
 }
 
 /*
@@ -18,8 +22,13 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
+async function checkUsernameFree(req, res, next) {
+  
+  if(){
 
+  } else {
+    next({ status: 401, message: "Username taken"})
+  }
 }
 
 /*
@@ -30,9 +39,7 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
-}
+function checkUsernameExists() {}
 
 /*
   If password is missing from req.body, or if it's 3 chars or shorter
@@ -42,8 +49,13 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
-}
+function checkPasswordLength() {}
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+};
